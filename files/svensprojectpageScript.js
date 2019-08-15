28ea6572bc00fdca48c8770c7c6752d70d72d821
Sven <sven92.h@web.de>
2019-08-15 15:29:04 +0200

    erweitert

--- files/svensprojectpageScript.js
+++ files/svensprojectpageScript.js
@@ -2,3 +2,7 @@ function fadeGitHubButton() {
     document.getElementById("testID").style.position = "absolute"
     document.getElementById("testID").style.left = "50px";
 }
+
+function scaleTest() {
+    document.getElementById("testID").style.fontSize = "50px";
+}
